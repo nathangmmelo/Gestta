fbe876e650c78e71121711620910fa94e8b0ba75
nathangmmelo <nathangmmelo@email.com>
2026-08-20 16:02:07 -0300

    feat add class produto e metodos

--- models/ingrediente.py
+++ models/ingrediente.py
@@ -2,11 +2,13 @@ class Ingrediente:
     def __init__(self, nome, preco_compra, qtd_comprada, qtd_usada):
         self.nome = nome
         self.preco_compra = preco_compra 
-        self.quantidade_comprada = qtd_comprada 
-        self.quantidade_usada = qtd_usada
+        self.qtd_comprada = qtd_comprada 
+        self.qtd_usada = qtd_usada
 
     def custo_unitario(self):
         return self.preco_compra / self.qtd_comprada
 
     def custo_na_receita(self):
         return self.custo_unitario() * self.qtd_usada
+    
+    
